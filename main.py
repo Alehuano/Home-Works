53099073884fee559aa7f17faa95d6fa33b23635
Alehuano <leos2001@mail.ru>
2024-05-27 20:55:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+#Задача «Длина слова»
+a="Проверка"
+print(len(a))
+#Задача «Суммы и разности»
+first=5
+second=3
+summa=first+second
+diff=first-second
+print(summa,diff)
+#Задача «Среднее арифметическое»
+first=7
+second=5
+third=3
+mean=(first+second+third)/3
+print(mean)
+#Задача «Простые строчки»
+first_string="Понедельник"
+second_string="Вторник"
+print(first_string,', ',second_string)
+#Задача «Сложная формула»
+a=8
+b=11
+c=6
+f=(((a*b)+(a*c))**3)/2
+print(f)
