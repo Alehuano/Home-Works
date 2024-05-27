1e043027cbf92b16b81805f8420655c83d11f78e
Alehuano <leos2001@mail.ru>
2024-05-27 21:20:02 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+home_works_count=12
+hours_used_count=1.5
+course_name='Python'
+time_to_one_work=hours_used_count/home_works_count
+print("Курс: ",course_name,", всего задач: ",home_works_count,", затрачено часов: ",hours_used_count,", среднее время выполнения: ",time_to_one_work," часа")
