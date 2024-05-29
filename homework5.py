a2ca8e46b00f83b32f09e0b19b72ae5c2a9c6015
Alehuano <leos2001@mail.ru>
2024-05-29 23:00:07 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var=5, 9, 1, "собака", True
+print(immutable_var)
+mutable_list=["лошадь", "корова",2,4,False]
+mutable_list[0]="свинья"
+mutable_list[2]=-15
+mutable_list.append("кошка")
+mutable_list.extend(["мышка", 7, True])
+print(mutable_list)
