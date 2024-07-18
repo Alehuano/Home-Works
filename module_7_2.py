d5071227d95eb6e53216d42bfbcdae18d3760793
Alehuano <leos2001@mail.ru>
2024-07-18 23:01:49 +0500

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -2,9 +2,9 @@ def custom_write(file_name, strings):
     strings_positions = {}
     file = open(file_name, 'w', encoding='utf-8')
     for i in strings:
-        position = file.tell()
-        file.write(i + '\n')
-        strings_positions[(len(strings_positions) + 1, position)] = i
+        current_position = file.tell()
+        file.write(f'{i}\n')
+        strings_positions[(len(strings_positions) + 1, current_position)] = i
     file.close()
     return strings_positions
 
